b01da77417616402d3f4ff3ba0cd30e9dbd6d06f
Shaked Avikazar <133217854+shaked0703@users.noreply.github.com>
2023-07-23 14:56:10 +0300

    Update testk8s.sh

--- testk8s.sh
+++ testk8s.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/bash -xe
 
-# Set the timeout value (in seconds)
-timeout=10
-
-# Make curl requests with a timeout
-curl -s --max-time $timeout 192.168.56.1:30300
-
-# Check patterns using grep and handle the cases
-if ! curl -s --max-time $timeout 192.168.56.1:30300 | grep -q Flask; then
-  echo "Error: Pattern 'Flask' not found"
-  exit 1
-fi
-
-if ! curl -s --max-time $timeout 192.168.56.1:30300 | grep -q App; then
-  echo "Error: Pattern 'App' not found"
-  exit 1
-fi
+curl -s 192.168.56.1:30300
