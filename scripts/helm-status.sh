19cc728f146b453427ce04011fecc820e93fae25
Shaked Avikazar <133217854+shaked0703@users.noreply.github.com>
2023-08-27 11:10:48 +0300

    Update helm-status.sh

--- scripts/helm-status.sh
+++ scripts/helm-status.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-command_output=$(helm status bucket-shaked 2>&1)
+command_output=$(helm status shaked 2>&1)
 
 expected_error="Error: release: not found"
 
 if [ "$command_output" = "$expected_error" ]; then
-    helm install bucket-shaked flask-app/
+    helm install shaked flask-app/
 else
-    helm upgrade --recreate-pods bucket-shaked flask-app/
+    helm upgrade --recreate-pods shaked flask-app/
 fi
