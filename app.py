ba93ee46f432bbe9418c989b451289580de32b8e
Shaked Avikazar <133217854+shaked0703@users.noreply.github.com>
2023-09-10 12:11:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Global variable to store visit count
+visit_count = 0
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    global visit_count
+    visit_count += 1
+    return render_template("index.html", visit_count=visit_count)
